c40b8f6834fe9a1303c3e70515aa5fe7b92ce163
y2jesan <ymjesanlemon@gmail.com>
2019-09-24 00:09:03 +0600

    1st commit from vs code

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,5 +1,9 @@
 ﻿import React from "react";
 
 export default function Blank() {
-  return <main id="mainContent" />;
+  return (
+    <main id="mainContent">
+      <h2>My 1st Commit From VS code</h2>
+    </main>
+  );
 }
