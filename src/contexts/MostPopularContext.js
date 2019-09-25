7e37ece2c2e548224ff7c9cc5071aa04707b3e30
y2jesan <ymjesanlemon@gmail.com>
2019-09-25 15:20:54 +0600

    use of useState

--- src/contexts/MostPopularContext.js
+++ src/contexts/MostPopularContext.js
@@ -1,38 +1,34 @@
-import React, { useState, createContext } from 'react';
+import React, { useState, createContext, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 export const MostPopularContext = createContext();
 
 export const MostPopularProvider = (props) => {
-	const [ seriesList, setSeriesList ] = useState([
-		{
-			Id: 1,
-			name: 'BB'
-		},
-		{
-			Id: 2,
-			name: 'GOT'
-		}
-	]);
+	const [ seriesList, setSeriesList ] = useState([]);
 	const [ mostPopularPageNo, setMostPopularPageNo ] = useState(0);
-	const mostPopularPage = 1; //useSelector((state) => state.mostPopularPage);
-	//const x = useSelector((state) => state.mostPopularPage);
+	//const mostPopularPage = 1; //useSelector((state) => state.mostPopularPage);
+	const mostPopularPage = useSelector((state) => state.mostPopularPage);
 	//console.log(x);
-	const loadMostPopularList = async () => {
-		const mostPopularUrl = `https://www.episodate.com/api/most-popular?page=${mostPopularPageNo}`;
-		const response = await fetch(mostPopularUrl);
-		const data = await response.json();
-		//console.log(data.tv_shows);
-		setSeriesList(data.tv_shows);
-		console.log(seriesList);
-	};
-	for (let i = 0; i < 2; i++) {
-		if (mostPopularPageNo !== mostPopularPage) {
-			console.log(mostPopularPage);
-			console.log(mostPopularPageNo);
-			setMostPopularPageNo(mostPopularPage);
-			loadMostPopularList();
-		}
-	}
+	useEffect(() => {
+		const loadMostPopularList = async () => {
+			const mostPopularUrl = `https://www.episodate.com/api/most-popular?page=${mostPopularPage}`;
+			const response = await fetch(mostPopularUrl).then();
+			const data = await response.json();
+			//console.log(data.tv_shows);
+			setSeriesList(data.tv_shows);
+			//console.log('seriesList: ' + seriesList);
+		};
+
+		loadMostPopularList();
+	}, []);
+
+	// for (let i = 0; i < 2; i++) {
+	// 	if (mostPopularPageNo !== mostPopularPage) {
+	// 		console.log(mostPopularPage);
+	// 		console.log(mostPopularPageNo);
+	// 		setMostPopularPageNo(mostPopularPage);
+	// 		loadMostPopularList();
+	// 	}
+	// }
 
 	return <MostPopularContext.Provider value={seriesList}>{props.children}</MostPopularContext.Provider>;
 };
