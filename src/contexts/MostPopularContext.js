e128c63fee735a7fa6cd993852a97e212b09d3f0
y2jesan <ymjesanlemon@gmail.com>
2019-09-26 09:26:56 +0600

    context description

--- src/contexts/MostPopularContext.js
+++ src/contexts/MostPopularContext.js
@@ -3,20 +3,22 @@ import { useSelector } from 'react-redux';
 export const MostPopularContext = createContext();
 
 export const MostPopularProvider = (props) => {
-	const [ seriesList, setSeriesList ] = useState([]);
-	//const [ mostPopularPageNo, setMostPopularPageNo ] = useState(0);
-	//const mostPopularPage = 1; //useSelector((state) => state.mostPopularPage);
-	const mostPopularPage = useSelector((state) => state.mostPopularPage);
-	useEffect(() => {
-		const loadMostPopularList = async () => {
-			const mostPopularUrl = `https://www.episodate.com/api/most-popular?page=${mostPopularPage}`;
-			const response = await fetch(mostPopularUrl).then();
-			const data = await response.json();
-			setSeriesList(data.tv_shows);
-		};
-
-		loadMostPopularList();
-	}, []);
+	const [ seriesList, setSeriesList ] = useState([]); //? Storing The Most Popular List Data
+	const mostPopularPage = useSelector((state) => state.mostPopularPage); //? Get The Page Number From Reducer
+	useEffect(
+		//? works Like ComponentDidMount
+		() => {
+			const loadMostPopularList = async () => {
+				//? Fetching The Data From Api
+				const mostPopularUrl = `https://www.episodate.com/api/most-popular?page=${mostPopularPage}`;
+				const response = await fetch(mostPopularUrl).then();
+				const data = await response.json();
+				setSeriesList(data.tv_shows);
+			};
+			loadMostPopularList();
+		},
+		[ mostPopularPage ] //? Dependency: useEffect will call everytime when 'mostPopularPage' is changed
+	);
 
 	return <MostPopularContext.Provider value={seriesList}>{props.children}</MostPopularContext.Provider>;
 };
